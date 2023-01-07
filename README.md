206a12a0723311bd4653636bfab82285e4e025ca
2Kelvin <rocketmancodes02@gmail.com>
2023-01-07 07:13:09 +0300

    lifted state up

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # sharing-state-between-components
+
+In this read, I learnt:
+
+- sharing state between components is called `lifting state up`
+- sharing the state means that the components' `state will always change together`
+- to share the state between two components, lift the state up to their `nearest common parent` and then `pass it down to both components as a prop`
+- that, you lift state up in 3 steps:
+  - [x] removing the state from the child's component
+  - [x] passing hardcorded state data from the common parent
+  - [x] adding state to the common parent then passing it down with the event handlers
+- once you lift up the state, `the component no longer has control over its state but its parent does`
+- the parent becomes the `source of truth` for both components' states
+- lifting state uo often changes the nature / what your state variable holds
+- the parent component has to explicitly allow its child components to change state by passing down an event handler to them as a prop
+- about `uncontrolled components`: components with their own local state which their parents can't write / access
+- `controlled components`: components whose state is fully controlled by their parents. They get their state passed down to them via props. They do not have local state because their state is stored up in their parent component.
+- controlled components are used when you want to coordinate components together. e.g. form inputs.
---
+++ app.js
@@ -0,0 +1,85 @@
+var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
+
+function Panel(_ref) {
+  var title = _ref.title,
+      children = _ref.children,
+      isActive = _ref.isActive,
+      onShow = _ref.onShow;
+
+  return React.createElement(
+    "section",
+    { className: "panel" },
+    React.createElement(
+      "h3",
+      null,
+      title
+    ),
+    isActive ? React.createElement(
+      "p",
+      null,
+      children
+    ) : React.createElement(
+      "button",
+      { onClick: onShow },
+      "Show Details"
+    )
+  );
+}
+
+function Parent() {
+  var _React$useState = React.useState(null),
+      _React$useState2 = _slicedToArray(_React$useState, 2),
+      activeIndex = _React$useState2[0],
+      setActiveIndex = _React$useState2[1];
+
+  return React.createElement(
+    "div",
+    null,
+    React.createElement(
+      "h2",
+      null,
+      "Almaty, Kazakhstan"
+    ),
+    React.createElement(
+      Panel,
+      {
+        title: "About",
+        isActive: activeIndex === 0,
+        onShow: function onShow() {
+          return setActiveIndex(0);
+        }
+      },
+      "With a population of about 2 million, Almaty is Kazakhstan's largest city. From 1929 to 1997, it was its capital city."
+    ),
+    React.createElement(
+      Panel,
+      {
+        title: "Etymology",
+        isActive: activeIndex === 1,
+        onShow: function onShow() {
+          return setActiveIndex(1);
+        }
+      },
+      "The name comes from ",
+      React.createElement(
+        "span",
+        { lang: "kk-KZ" },
+        "\u0430\u043B\u043C\u0430"
+      ),
+      ", the Kazakh word for \"apple\" and is often translated as \"full of apples\". In fact, the region surrounding Almaty is thought to be the ancestral home of the apple, and the wild ",
+      React.createElement(
+        "i",
+        { lang: "la" },
+        "Malus sieversii"
+      ),
+      " is considered a likely candidate for the ancestor of the modern domestic apple."
+    )
+  );
+}
+
+export default function App() {
+  return React.createElement(Parent, null);
+}
+
+var root = ReactDOM.createRoot(document.getElementById("rootNode"));
+root.render(React.createElement(App, null));
